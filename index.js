c68241bdaac239effed517454cb5bf74628fc4fb
Ashish Mittal <ashishmittal2047>
2020-07-14 22:47:18 +0530

    Setting up express router

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router
+app.use('/',require('./routes'));       //we have directly written require here, instead of storing require in a variable and then writing.We also don't need to mention index.js in routes as it is picked by default
 
 app.listen(port,function(err){
     if(err)
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const router = express.Router();
+console.log('You know what, express router is being used!');
+module.exports = router;
