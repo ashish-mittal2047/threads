fd39386673f967aaa2664a028763f1c597ee2539
Ashish Mittal <ashishmittal2047>
2020-07-15 21:37:31 +0530

    setting up view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //use express router
 app.use('/',require('./routes'));       //we have directly written require here, instead of storing require in a variable and then writing.We also don't need to mention index.js in routes as it is picked by default
 
+
+//setting up view engine
+app.set('view engine','ejs');
+app.set('views','./views');             //instead of using path.join and __dirname, we write relative path directly
 app.listen(port,function(err){
     if(err)
     {
