04de8be8075c38b0af4c1eb4bbe63caab42bbf05
Ashish Mittal <ashishmittal2047>
2020-07-16 02:10:06 +0530

    add user sign in and sign up views and set up controllers and routers accordingly

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,17 @@
 module.exports.profile = function(req, res){
-    res.render('user_profile',{
+    return res.render('user_profile',{
         title: "Profile"
+    });
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up.ejs',{
+        title: "Sign Up"
+    });
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in.ejs',{
+        title: "Sign In"
     });
 }
