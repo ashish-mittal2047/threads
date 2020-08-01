0e66b568a4abc9f3010eb364d4cf8a4773c490c7
Ashish Mittal <ashishmittal2047>
2020-08-02 00:58:01 +0530

    create post schema in models and create a form on home page to post a post

---
+++ models/post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+//There is 1 to Many relationship between user and post
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required:true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},{
+    timestamps: true
+});
+
+const Post = mongoose.model('Post',postSchema);
+module.exports = Post;
