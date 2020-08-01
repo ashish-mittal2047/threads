6c7bcdde1be7397021b1dc560f34e5149bc4d6ec
Ashish Mittal <ashishmittal2047>
2020-08-02 01:18:44 +0530

    create an action in controller to handle posts i.e a function that actually creates post in database and finally setup related router

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ console.log('ROUTER LOADED!');
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
-
+router.use('/posts',require('./posts'));
 //for any further routes, access from here
 // router.use('/routername',require('./routerfile'));
 module.exports = router;
