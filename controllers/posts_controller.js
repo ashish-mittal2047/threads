cc31d2cefc26f9f819d645e31070a88e270dc816
Ashish Mittal <ashishmittal2047>
2020-08-02 01:53:02 +0530

    display posts on home page and add suitable controller for it(modify home_controller)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,8 @@
 const Post = require('../models/post');
 
+
+//this method Post.create creates a schema object with contents from request body that is coming from form content and its user
+//still the post form is visible to even an unsigned user, so we need to modify it
 module.exports.create = function(req,res){
     Post.create({
         content: req.body.content,
