4627d400ec94eab5804dc9ba5cbd65e2fa20356a
Ashish Mittal <ashishmittal2047>
2020-08-02 15:13:40 +0530

    Adding delete post functionality and deleting all associated comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const Comment = require('../models/comment');
 
 //this method Post.create creates a schema object with contents from request body that is coming from form content and its user
 //still the post form is visible to even an unsigned user, so we need to modify it
@@ -13,5 +13,24 @@ module.exports.create = function(req,res){
             return;
         }
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id, function(err, post){
+        //.id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+            //becoz only the user who made the post should be allowed to delete it and also the same user should be logged in
+            //also we need to remove all comments associated with the post
+            Comment.deleteMany({post: req.params.id},function(err){
+                if(err){console.log('Error');}
+                return res.redirect('back');
+            });
+        }
+        else{
+            //if signed-in user doesn't match with the one who wrote the post, then return back
+            return res.redirect('back');
+        }
     });
 }
