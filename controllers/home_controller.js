82b74db7a4eabe5bdeabb5ec9e9e095e3c0e757b
Ashish Mittal <ashishmittal2047>
2020-08-03 02:41:09 +0530

    convert code in home controller from callback format to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){      //we write async here so that we can use async await syntax
     
     // Post.find({},function(err, posts){
     //     return res.render('home',{
@@ -11,25 +11,48 @@ module.exports.home = function(req,res){
     //     });
 
     // });
+    //Commented code above this line is worthless.
     //Instead of simply using this, we pre-populate user so that we can display name and other info of author instead of objectId 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+    // Post.find({})
+    // .populate('user')
+    // .populate({
+    //     path:'comments',
+    //     populate: {
+    //         path: 'user'
+    //     }
+    // })
+    // .exec(function(err, posts){             
 
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: "Threads | Home",
-                posts: posts,
-                all_users: users
-            });
+    //     User.find({},function(err,users){
+    //         return res.render('home',{
+    //             title: "Threads | Home",
+    //             posts: posts,
+    //             all_users: users
+    //         });
+    //     });
+        
+    // });
+    //We use async await to present our above code in a cleaner format
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate: {
+                path: 'user'
+            }
         });
         
-    });
-    
+        let users = await User.find({}); //await is used to synchronize asynchonous calls so that they happen only after each other in a specific order
+        //async await is much better as compared to using callbacks and promises.
+        return res.render('home',{
+            title: "Threads | Home",
+            posts: posts,
+            all_users: users
+        });
+
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
