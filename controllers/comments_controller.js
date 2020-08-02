f52fe82b26ba768b7b5506af47489e342fc16db9
Ashish Mittal <ashishmittal2047>
2020-08-02 15:52:07 +0530

    add individual comment delete functionality alongwith related authorization

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -18,10 +18,27 @@ module.exports.create = function(req,res){
                     return;
                 }
                 post.comments.push(comment);    //here, it is modified only on server
-                post.save();    //this permanentl modifies comment array by modifying in database
+                post.save();    //this permanently modifies comment array by modifying in database
             
                 res.redirect('/');
             });
+        }
+    });
+}
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(comment.user == req.user.id){
+            //here we store objectId of post to which comment belongs, before deleting comment so that we can go to that ppost and delete this comment's id from comment array in that post
+            let postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id }},function(err,post){ //here $pull is an update operation. It pulls out(deletes) all instances of a value that match a specified condition. Here condition is, a value with req.params.id in comments array
+                return res.redirect('back')
+            });
+        }
+        else{
+            //if user isn't the one who created that comment, then he is not allowed to delete comment and thus redirected back
+            return res.redirect('back');
         }
     });
 }
--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const commentsController = require('../controllers/comments_controller');
 
 router.post('/create', passport.checkAuthentication,commentsController.create);
-
+router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 module.exports = router;
 
 //Here we defined a route for comment form
