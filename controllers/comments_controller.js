9f9b355ae6cf31822f8d4f5c440bb9d3d1fe8ad9
Ashish Mittal <ashishmittal2047>
2020-08-04 15:33:40 +0530

    changing code in comments_controller and posts_controller to async await from simple callbacks

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,42 +3,88 @@ const Post = require('../models/post');
 
 //Note: req.body.post gives the data sent by input field with name 'post' in the comment form
 // Similarly, req.body.content
-module.exports.create = function(req,res){
-    Post.findById(req.body.post, function(err,post){
+// module.exports.create = function(req,res){
+//     Post.findById(req.body.post, function(err,post){
+//         if(post)
+//         {
+//             Comment.create({
+//                 content: req.body.content,
+//                 post: req.body.post,
+//                 user: req.user._id
+//             }, function(err, comment){
+//                 if(err)
+//                 {
+//                     console.log('Error');
+//                     return;
+//                 }
+//                 post.comments.push(comment);    //here, it is modified only on server
+//                 post.save();    //this permanently modifies comment array by modifying in database
+            
+//                 res.redirect('/');
+//             });
+//         }
+//     });
+// }
+
+module.exports.create = async function(req,res){
+    try
+    {
+        let post = await Post.findById(req.body.post);
         if(post)
         {
-            Comment.create({
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
-                if(err)
-                {
-                    console.log('Error');
-                    return;
-                }
-                post.comments.push(comment);    //here, it is modified only on server
-                post.save();    //this permanently modifies comment array by modifying in database
-            
-                res.redirect('/');
             });
+            post.comments.push(comment);    //here, it is modified only on server
+            post.save();    //this permanently modifies comment array by modifying in database
+            res.redirect('/');
         }
-    });
+    }
+    catch(err)
+    {
+        console.log('Error');
+        return;
+    }
 }
 
-module.exports.destroy = function(req,res){
-    Comment.findById(req.params.id, function(err, comment){
+// module.exports.destroy = function(req,res){
+//     Comment.findById(req.params.id, function(err, comment){
+//         if(comment.user == req.user.id){
+//             //here we store objectId of post to which comment belongs, before deleting comment so that we can go to that ppost and delete this comment's id from comment array in that post
+//             let postId = comment.post;
+//             comment.remove();
+//             Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id }},function(err,post){ //here $pull is an update operation. It pulls out(deletes) all instances of a value that match a specified condition. Here condition is, a value with req.params.id in comments array
+//                 return res.redirect('back')
+//             });
+//         }
+//         else{
+//             //if user isn't the one who created that comment, then he is not allowed to delete comment and thus redirected back
+//             return res.redirect('back');
+//         }
+//     });
+// }
+
+module.exports.destroy = async function(req,res){
+    try
+    {
+        let comment = await Comment.findById(req.params.id);
         if(comment.user == req.user.id){
             //here we store objectId of post to which comment belongs, before deleting comment so that we can go to that ppost and delete this comment's id from comment array in that post
             let postId = comment.post;
             comment.remove();
-            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id }},function(err,post){ //here $pull is an update operation. It pulls out(deletes) all instances of a value that match a specified condition. Here condition is, a value with req.params.id in comments array
-                return res.redirect('back')
-            });
+            await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id }});          //here $pull is an update operation. It pulls out(deletes) all instances of a value that match a specified condition. Here condition is, a value with req.params.id in comments array
+            return res.redirect('back');
         }
         else{
             //if user isn't the one who created that comment, then he is not allowed to delete comment and thus redirected back
             return res.redirect('back');
         }
-    });
+    }
+    catch(err)
+    {
+        console.log('Error');
+        return;
+    }
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,34 +3,76 @@ const Comment = require('../models/comment');
 
 //this method Post.create creates a schema object with contents from request body that is coming from form content and its user
 //still the post form is visible to even an unsigned user, so we need to modify it
-module.exports.create = function(req,res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){
-            console.log('error in creating a post');
-            return;
-        }
+//although below code has just one level of callback(not nested), so there is no need for aysnc await but we still do it to understand
+// module.exports.create = function(req,res){
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
+//     }, function(err, post){
+//         if(err){
+//             console.log('error in creating a post');
+//             return;
+//         }
+//         return res.redirect('back');
+//     });
+// }
+
+
+
+module.exports.create = async function(req,res){
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
         return res.redirect('back');
-    });
+    }
+    catch(err){
+        console.log('error occurred in creating a post');
+    }   
+    //Note: try catch is not compulsory but it is usually good to catch errors just as we do in callback
 }
 
-module.exports.destroy = function(req,res){
-    Post.findById(req.params.id, function(err, post){
-        //.id means converting the object id into string
+
+// module.exports.destroy = function(req,res){
+//     Post.findById(req.params.id, function(err, post){
+//         //.id means converting the object id into string
+//         if(post.user == req.user.id){
+//             post.remove();
+//             //becoz only the user who made the post should be allowed to delete it and also the same user should be logged in
+//             //also we need to remove all comments associated with the post
+//             Comment.deleteMany({post: req.params.id},function(err){
+//                 if(err){console.log('Error');}
+//                 return res.redirect('back');
+//             });
+//         }
+//         else{
+//             //if signed-in user doesn't match with the one who wrote the post, then return back
+//             return res.redirect('back');
+//         }
+//     });
+// }
+
+module.exports.destroy = async function(req,res){
+    let post = await Post.findById(req.params.id);
+    try
+    {
         if(post.user == req.user.id){
             post.remove();
             //becoz only the user who made the post should be allowed to delete it and also the same user should be logged in
             //also we need to remove all comments associated with the post
-            Comment.deleteMany({post: req.params.id},function(err){
-                if(err){console.log('Error');}
-                return res.redirect('back');
-            });
+            await Comment.deleteMany({post: req.params.id});
+            return res.redirect('back');
         }
         else{
             //if signed-in user doesn't match with the one who wrote the post, then return back
             return res.redirect('back');
         }
-    });
+    }
+    catch(err)
+    {
+        console.log('Error');
+        return;
+    }
+    
 }
